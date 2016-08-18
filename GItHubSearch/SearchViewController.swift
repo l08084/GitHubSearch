1bed5e7028c8a1bb4f6f065c69453d59675d3181
l08084 <l08084@shibaura-it.ac.jp>
2016-08-18 14:44:15 +0900

    new SearchViewController

---
+++ GItHubSearch/SearchViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SearchViewController.swift
+//  GItHubSearch
+//
+//  Created by 中安拓也 on 2016/08/18.
+//  Copyright © 2016年 l08084. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchViewController: UITableViewController {
+    
+}
